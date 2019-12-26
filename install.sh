eabc0e820123d1f107e045ef74771323dda2c299
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2019-12-26 17:04:43 +0200

    revisio launch-5

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@ tag="master"
 ver="v0.0.0"
 host="dev.184-172-214-143.nip.io"
 
-helm upgrade --install $name  --debug --dry-run ./wiki \
+helm upgrade --install $name  --debug  ./wiki \
     --force \
     --wait \
-    --namespace $ns \
-    --set namespace=$ns \
-    --set image.tag=$tag \
-    --set appVer=$ver \
-    --set ingress.hostName=$host \
-    --set-string ingress.tls[0].hosts[0]=$host \
-    --set-string ingress.tls[0].secretName=acme-$name-tls \
+   --namespace $ns \
+            --set image.tag=$tag \
+            --set appVer=$tag \
+            --set-string ingress.hostName="${name}.${host}" \
+            --set-string ingress.tls[0].hosts[0]="${name}.${host}" \
+            --set-string ingress.tls[0].secretName="acme-${name}-tls" 
    
