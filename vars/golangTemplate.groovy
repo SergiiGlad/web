55cf4b005999ba33f9affc824467e22940cff706
Sergii Gladchenko <sgladc@macmini576.ddns.softservecom.com>
2020-01-13 10:59:01 +0200

    dotNet

--- vars/golangTemplate.groovy
+++ vars/golangTemplate.groovy
@@ -1,25 +1,13 @@
 def call(String podLabel, code) { podTemplate(
-    cloud: 'ibm kubernetes',
+    cloud: 'kubernetes-dev',
     namespace: 'jenkins',
     label: podLabel,
     containers: [
       containerTemplate(
-        name: golang,
-        image: golang:1.13.0-alpine,
-        command: 'cat',
+        name: 'dotnet',
+        image: 'microsoft/dotnet',
         ttyEnabled: true,
-        name: docker-dind
-        image: docker:stable-dind
-        securityContext:
-          privileged: true
-        name: helm
-        image: lachlanevenson/k8s-helm:v2.16.1
-        command: 'cat',
-        ttyEnabled: true,
-        name: kubectl
-        image: lachlanevenson/k8s-kubectl:v1.16.4
-        command: 'cat',
-        ttyEnabled: true)
+        command: 'cat')
 ]) {
 code() }
 }
